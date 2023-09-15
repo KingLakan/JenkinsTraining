98f16b11a78cf752a60d55d1460ce409395747f6
Lakan <david.pb.samuelsson@gmail.com>
2023-09-15 13:45:08 +0200

    Update

---
+++ Build/CMakeFiles/Jenkins.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Jenkins.dir/src/main.cpp.obj"
+  "Jenkins.exe"
+  "Jenkins.exe.manifest"
+  "Jenkins.pdb"
+  "libJenkins.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Jenkins.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
